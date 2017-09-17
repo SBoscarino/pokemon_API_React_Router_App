1a6bb8bb5430f12145d1144ffbee3f47c4d1e70e
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-09-17 15:37:51 -0500

    still working on this component

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -7,62 +7,62 @@ constructor(){
 
   this.state = {
     pokemonData: [],
-    value: 'bulbasaur'
+    value: '',
+    name: '',
+    weight: '',
+    height: '',
+    order: '',
+    base_experience: '',
+    types: []
   }
   this.handleChange = this.handleChange.bind(this);
   this.handleSubmit = this.handleSubmit.bind(this);
 }
 
 handleChange(event) {
+  // console.log('handleChange', event.target.value)
   this.setState({value: event.target.value});
 }
 
 handleSubmit(event) {
-  console.log('handleSubmit is happening!!');
+  console.log('Pokemon name submitted!!');
   fetch('http://pokeapi.co/api/v2/pokemon/' + this.state.value)
   .then(results => {
     return results.json();
-  }).then(data => {
-    console.log('data in choose a pokemon textarea', data);
-    this.setState({pokemonData: data.results});
+  }).then(pokemon => {
+    console.log('data in handleSubmit:', pokemon);
+    this.setState({pokemonData: pokemon});
  });
-   event.preventDefault();
+ event.preventDefault();
  }
 
-componentDidMount(){
-  console.log('state in render',this.state);
-     fetch('http://pokeapi.co/api/v2/pokemon/' + this.state.value)
-     .then(results => {
-       return results.json();
-     }).then(data => {
-       console.log('data in choose a pokemon textarea', data);
-       this.setState({pokemonData: data.results});
-    });
-}
-
-
-
-    render(){
-    return ( <div>
-        <form onSubmit={this.handleSubmit}>
-          <h1>Search for a Pokemon:</h1>
-          <textarea value={this.state.value} onChange={this.handleChange}></textarea>
-          <input type="submit" value="Submit" />
-        </form>
-          <div>
-            <div className="sprite">
-              <img  src={this.state.pokemonData.sprites.front_default}/>
-            </div>
-            <div className="info">
-            <h1>{this.state.pokemonData.name}</h1>
-            <p>Pokedex Number: {this.state.pokemonData.order}</p>
-            <p>Height: {this.state.pokemonData.height}</p>
-            <p>Weight: {this.state.pokemonData.weight}</p>
-            <p>Base Experience:{this.state.pokemonData.base_experience}</p>
-            <p>Type:{this.state.pokemonData.types[1]}, {this.state.pokemonData.types[2]}</p>
-            </div>
+  render(){
+    let PokemonPopUp;
+    if (this.state.pokemonData.name) {
+      let PokemonPopUp = <div>
+          <div className="sprite">
+            <img src='{this.props.pokemonData.sprites[front_default]}'/>
           </div>
-      </div>
+          <div className="info">
+          <h1>{this.state.pokemonData.name}</h1>
+          <p>Pokedex Number: {this.state.pokemonData.order}</p>
+          <p>Height: {this.state.pokemonData.height}</p>
+          <p>Weight: {this.state.pokemonData.weight}</p>
+          <p>Base Experience:{this.state.pokemonData.base_experience}</p>
+          </div>
+        </div>
+    } else {
+      PokemonPopUp = <p>Search for a Pokemon to learn something new! Example: pikachu</p>
+    }
+  return (
+    <div>
+      <form onSubmit={this.handleSubmit}>
+        <h1>Search for a Pokemon:</h1>
+        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        <input type="submit" value="Submit" />
+      </form>
+      {PokemonPopUp}
+    </div>
     )
   }
 }
